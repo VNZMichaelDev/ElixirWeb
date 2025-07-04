e8f80c44b0f7419fbc038a4909d2badc3bbdcfcb
VNZMichaelDev <maikermicha15@gmail.com>
2025-07-04 13:13:16 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 // Global variables
 let isMobileMenuOpen = false
-let Tawk_API // Declare Tawk_API variable
-
-// Live Chat function
-function openLiveChat() {
-  if (typeof Tawk_API !== "undefined" && Tawk_API.maximize) {
-    Tawk_API.maximize()
-  } else {
-    // Fallback to WhatsApp if Tawk.to is not loaded
-    openWhatsApp()
-  }
-}
 
 // WhatsApp function - General
 function openWhatsApp() {
@@ -361,3 +350,15 @@ function optimizeAnimations() {
 }
 
 document.addEventListener("DOMContentLoaded", optimizeAnimations)
+
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/684e78b14aea20190b635e6c/1itp8etlj';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+<!--End of Tawk.to Script-->
